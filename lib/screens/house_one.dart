6d69af4397ffa10368f026f4da4ae5f1f15bed41
Ankit Jangid <ajangid663@gmail.com>
2022-10-08 12:47:39 +0530

    SingleChildScrollView fixed

--- lib/screens/house_one.dart
+++ lib/screens/house_one.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:ui_test/custom/BorderBox.dart';
 import 'package:ui_test/custom/option_button.dart';
-import 'package:ui_test/utils/constants.dart';
 import 'package:ui_test/utils/widget_functions.dart';
 
 class HouseScreen extends StatelessWidget {
@@ -25,7 +24,8 @@ class HouseScreen extends StatelessWidget {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Image(image: AssetImage('assets/images/image_1.jpg')),
+                      const Image(
+                          image: AssetImage('assets/images/image_1.jpg')),
                       addVerticalSpace(30),
                       Padding(
                         padding: sidePadding,
@@ -39,7 +39,7 @@ class HouseScreen extends StatelessWidget {
                                   '\$200,000',
                                   style: themeData.textTheme.headline1,
                                 ),
-                                Text('\$Jenison, MI 49428, SF')
+                                const Text('\$Jenison, MI 49428, SF')
                               ],
                             ),
                             BorderBox(
@@ -61,46 +61,34 @@ class HouseScreen extends StatelessWidget {
                       ),
                       addVerticalSpace(25),
                       SingleChildScrollView(
-                        physics: BouncingScrollPhysics(),
-                        scrollDirection: Axis.horizontal,
-                        child: Column(
-                          children: [
-                            Row(
-                                children: ['1416', '4', '2', '1']
-                                    .map((e) => Padding(
-                                          padding: const EdgeInsets.only(
-                                              left: padding),
-                                          child: BorderBox(
-                                            width: 100,
-                                            height: 100,
-                                            child: Text(
-                                              e,
-                                              style:
-                                                  themeData.textTheme.headline2,
-                                            ),
-                                          ),
-                                        ))
-                                    .toList()),
-                                    addVerticalSpace(15),
-                            Row(
-                                children: [
-                              'Square Foot',
-                              'Bedrooms',
-                              'Bathrooms',
-                              'Garage'
-                            ]
-                                    .map((e) => SizedBox(
-                                        width: 120,
-                                        height: 60,
-                                        child: Text(e, style: themeData.textTheme.headline5,)))
-                                    .toList())
-                          ],
-                        ),
-                      ),
+                          physics: const BouncingScrollPhysics(),
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: const [
+                              InfoTile(
+                                content: '1416',
+                                name: 'Square Foot',
+                              ),
+                              InfoTile(
+                                content: '5',
+                                name: 'Fuck Rooms',
+                              ),
+                              InfoTile(
+                                content: '1',
+                                name: 'Helipad',
+                              ),
+                              InfoTile(
+                                content: '5',
+                                name: 'Car Garage',
+                              ),
+                            ],
+                          )),
+                      addVerticalSpace(20),
                       const Padding(
                         padding: sidePadding,
-                        child: Text('Lorem Ipsum is simply dummy text of the pr, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unch , when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unch ',
-                        textAlign: TextAlign.justify,
+                        child: Text(
+                          'Lorem Ipsum is simply dummy text of the pr,  make a type sh  uk fj ghjghj tyjhg pecimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unch , when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unch ',
+                          textAlign: TextAlign.justify,
                         ),
                       )
                     ],
@@ -112,7 +100,7 @@ class HouseScreen extends StatelessWidget {
                           padding: sidePadding,
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
+                            children: const [
                               BorderBox(child: Icon(Icons.arrow_back_outlined)),
                               BorderBox(child: Icon(Icons.favorite_border))
                             ],
@@ -134,11 +122,41 @@ class HouseScreen extends StatelessWidget {
                           )
                         ],
                       ),
-                      
                     ],
                   ),
                 ],
               ))),
     );
+  }
+}
+
+class InfoTile extends StatelessWidget {
+  final String? content;
+  final String? name;
+  final EdgeInsets? padding;
+  const InfoTile({super.key, this.content, this.name, this.padding});
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final ThemeData themeData = Theme.of(context);
+    final titleSize = size.width * 0.25;
+    return Container(
+        margin: const EdgeInsets.only(left: 25),
+        padding: padding ?? const EdgeInsets.all(8.0),
+        child: Column(children: [
+          BorderBox(
+              width: titleSize,
+              height: titleSize,
+              child: Text(
+                content!,
+                style: themeData.textTheme.headline3,
+              )),
+          addVerticalSpace(15),
+          Text(
+            name!,
+            style: themeData.textTheme.headline5,
+          )
+        ]));
   }
 }
